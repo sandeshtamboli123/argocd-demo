2d34177b65576d85d8bbdab1c222bdec2023a952
sandeshtamboli123 <90719183+sandeshtamboli123@users.noreply.github.com>
2023-01-05 15:55:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ if (process.env.ENVIRONMENT === 'prod') {
 const server = http.createServer((_, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World to devops team');
+  res.end('welcome to cognologix');
 });
 
 
